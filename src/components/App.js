cf9b895cf2daf4915088ef1d4924645e7a9f6bce
ivanico <55762391+ivanico@users.noreply.github.com>
2020-09-28 10:13:22 +0200

    added player

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
 
 export class App extends React.Component{
   render(){
@@ -11,8 +12,12 @@ export class App extends React.Component{
         <h2>EcoBang</h2>
       </div>
       <div>
-        <p>Intro Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-
+        <p>
+          Intro Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        </p>
+      </div>
+      <div>
+        <ReactPlayer url="https://www.youtube.com/watch?v=KXw8CRapg7k&ab_channel=QueenVEVO" controls={true} />
       </div>
     </div>
     )
