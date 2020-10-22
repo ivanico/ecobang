917ce47391247e48b711eb2b393638fbcf83209f
ivanico <trudno.pile3@gmail.com>
2020-10-23 01:27:30 +0200

    dsa

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+export default class Home extends React.Component{
+    render() {
+        return(
+            <div>
+                <div>
+                 <div id="title">
+                     <h2>EcoBang</h2>
+                </div>
+                <div id="container">
+                    <p>
+                    Intro Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </p>
+             </div>
+              <div id="video">
+                    <ReactPlayer url="https://www.youtube.com/watch?v=6k7esY7g4Y8&ab_channel=artemtroitcky" controls={true} id="video-player" />
+                    <span id="video-title">Video Title</span>
+                    <p id="description">
+                        Intro Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./assets/style.css";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import {App} from './components/App';
 
-
-ReactDOM.render( <App />, document.getElementById('root')
+ReactDOM.render(
+    <BrowserRouter>
+        <Route path="/" component={App} />
+    </BrowserRouter>, 
+    document.getElementById('root')
 );
-
