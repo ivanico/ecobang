4014d96b333a3973f5eaef00dc5631e51426f06f
ivanico <55762391+ivanico@users.noreply.github.com>
2021-02-04 18:17:59 +0100

    slaider

--- src/components/EcoVideo.js
+++ src/components/EcoVideo.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default class EcoVideo extends React.Component{
     render() {
+        var settings = {
+            dots: true,
+            speed: 500,
+            slidesToShow: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            slidesToScroll: 1,
+            pauseOnHover: true
+          };
         return(
-            <div>
-                EcoVideo
-                <ReactPlayer className="video-player"  url="https://www.youtube.com/watch?v=6k7esY7g4Y8&ab_channel=artemtroitcky"/>
-                <ReactPlayer  url="https://www.youtube.com/watch?v=XAi3VTSdTxU&ab_channel=michaeljacksonVEVO"/>
-            </div>
+                <Slider {...settings}>
+                <div>
+                    <ReactPlayer className="video-player"  url="https://www.youtube.com/watch?v=6k7esY7g4Y8&ab_channel=artemtroitcky"/>
+                </div>
+                <div>
+                    <ReactPlayer  url="https://www.youtube.com/watch?v=6k7esY7g4Y8&ab_channel=artemtroitcky"/>
+                </div>
+                <div>
+                    <ReactPlayer  url="https://www.youtube.com/watch?v=6k7esY7g4Y8&ab_channel=artemtroitcky"/>
+                </div>
+                <div>
+                    <ReactPlayer  url="https://www.youtube.com/watch?v=6k7esY7g4Y8&ab_channel=artemtroitcky"/>
+                </div>
+                <div>
+                    <ReactPlayer  url="https://www.youtube.com/watch?v=6k7esY7g4Y8&ab_channel=artemtroitcky"/>
+                </div>
+                </Slider>
         )
     }
 }
